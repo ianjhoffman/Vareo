f9d2536f23c3c1f7bc187f6606e12415c3648cf5
Ian Hoffman <ijh6@cornell.edu>
2023-09-20 23:41:08 -0400

    Panel design woohoo

--- src/plugin.cpp
+++ src/plugin.cpp
@@ -1,6 +1,5 @@
 #include "plugin.hpp"
 
-
 Plugin* pluginInstance;
 
 
@@ -8,7 +7,7 @@ void init(Plugin* p) {
 	pluginInstance = p;
 
 	// Add modules here
-	// p->addModel(modelMyModule);
+	p->addModel(modelVareo);
 
 	// Any other plugin initialization may go here.
 	// As an alternative, consider lazy-loading assets and lookup tables when your module is created to reduce startup times of Rack.
--- src/vareo.cpp
+++ src/vareo.cpp
@@ -15,19 +15,19 @@ struct Vareo : Module {
 		PARAMS_LEN
 	};
 	enum InputId {
-		BLEND_INPUT_INPUT,
-		DELAY_INPUT_INPUT,
-		R_AUDIO_INPUT_INPUT,
-		REC_INPUT_INPUT,
-		SPEED_INPUT_INPUT,
-		L_AUDIO_INPUT_INPUT,
 		JUMP_INPUT_INPUT,
+		L_AUDIO_INPUT_INPUT,
+		SPEED_INPUT_INPUT,
+		REC_INPUT_INPUT,
+		R_AUDIO_INPUT_INPUT,
+		DELAY_INPUT_INPUT,
+		BLEND_INPUT_INPUT,
 		INPUTS_LEN
 	};
 	enum OutputId {
-		R_AUDIO_OUTPUT_OUTPUT,
-		L_AUDIO_OUTPUT_OUTPUT,
 		END_OUTPUT_OUTPUT,
+		L_AUDIO_OUTPUT_OUTPUT,
+		R_AUDIO_OUTPUT_OUTPUT,
 		OUTPUTS_LEN
 	};
 	enum LightId {
@@ -39,28 +39,56 @@ struct Vareo : Module {
 
 	Vareo() {
 		config(PARAMS_LEN, INPUTS_LEN, OUTPUTS_LEN, LIGHTS_LEN);
-		configParam(REC_BUTTON_PARAM, 0.f, 1.f, 0.f, "");
-		configParam(JUMP_BUTTON_PARAM, 0.f, 1.f, 0.f, "");
-		configParam(SPEED_KNOB_PARAM, 0.f, 1.f, 0.f, "");
-		configParam(SPEED_ATTENUVERTER_PARAM, 0.f, 1.f, 0.f, "");
-		configParam(DELAY_KNOB_PARAM, 0.f, 1.f, 0.f, "");
-		configParam(JUMP_DIR_SWITCH_PARAM, 0.f, 1.f, 0.f, "");
-		configParam(BLEND_MODE_SWITCH_PARAM, 0.f, 1.f, 0.f, "");
-		configParam(BLEND_KNOB_PARAM, 0.f, 1.f, 0.f, "");
-		configParam(JUMP_MODE_SWITCH_PARAM, 0.f, 1.f, 0.f, "");
-		configInput(BLEND_INPUT_INPUT, "");
-		configInput(DELAY_INPUT_INPUT, "");
-		configInput(R_AUDIO_INPUT_INPUT, "");
-		configInput(REC_INPUT_INPUT, "");
-		configInput(SPEED_INPUT_INPUT, "");
-		configInput(L_AUDIO_INPUT_INPUT, "");
-		configInput(JUMP_INPUT_INPUT, "");
-		configOutput(R_AUDIO_OUTPUT_OUTPUT, "");
-		configOutput(L_AUDIO_OUTPUT_OUTPUT, "");
-		configOutput(END_OUTPUT_OUTPUT, "");
+		configButton(REC_BUTTON_PARAM, "Record Button");
+		configButton(JUMP_BUTTON_PARAM, "Jump Button");
+		configParam(SPEED_KNOB_PARAM, -1.f, 1.f, 0.f, "Speed");
+		configParam(SPEED_ATTENUVERTER_PARAM, -1.f, 1.f, 0.f, "Speed CV Attenuverter");
+		configParam(DELAY_KNOB_PARAM, 0.f, 1.f, 0.f, "Read Delay");
+		configSwitch(JUMP_DIR_SWITCH_PARAM, 0.f, 1.f, 0.f, "Jump Direction", {"Forward", "Backward"});
+		configSwitch(BLEND_MODE_SWITCH_PARAM, 0.f, 1.f, 0.f, "Blend Mode", {"Add", "Average"});
+		configParam(BLEND_KNOB_PARAM, 0.f, 1.f, 0.f, "Write Blend");
+		configSwitch(JUMP_MODE_SWITCH_PARAM, 0.f, 1.f, 0.f, "Jump Mode", {"Start of Recording", "Start of Tape"});
+		configInput(BLEND_INPUT_INPUT, "Write Blend CV Input");
+		configInput(DELAY_INPUT_INPUT, "Read Delay CV Input");
+		configInput(R_AUDIO_INPUT_INPUT, "Right Audio Input");
+		configInput(REC_INPUT_INPUT, "Record Toggle Gate Input");
+		configInput(SPEED_INPUT_INPUT, "Speed CV Input");
+		configInput(L_AUDIO_INPUT_INPUT, "Left Audio Input");
+		configInput(JUMP_INPUT_INPUT, "Jump Trigger Input");
+		configOutput(R_AUDIO_OUTPUT_OUTPUT, "Right Audio Output");
+		configOutput(L_AUDIO_OUTPUT_OUTPUT, "Left Audio Output");
+		configOutput(END_OUTPUT_OUTPUT, "End Of Recording Output");
+		configBypass(L_AUDIO_INPUT_INPUT, L_AUDIO_OUTPUT_OUTPUT);
+		configBypass(R_AUDIO_INPUT_INPUT, R_AUDIO_OUTPUT_OUTPUT);
 	}
 
+	float phase = 0.f;
+	float blinkPhase = 0.f;
+
 	void process(const ProcessArgs& args) override {
+		// Compute the frequency from the pitch parameter and input
+		float pitch = params[SPEED_KNOB_PARAM].getValue();
+		float modAmt = params[SPEED_ATTENUVERTER_PARAM].getValue();
+		pitch += modAmt * inputs[SPEED_INPUT_INPUT].getVoltage();
+		// The default frequency is C4 = 261.6256f
+		float freq = dsp::FREQ_C4 * std::pow(2.f, pitch);
+
+		// Accumulate the phase
+		phase += freq * args.sampleTime;
+		if (phase >= 1.f)
+			phase -= 1.f;
+
+		// Compute the sine output
+		float sine = std::sin(2.f * M_PI * phase);
+		// Audio signals are typically +/-5V
+		// https://vcvrack.com/manual/VoltageStandards
+		outputs[R_AUDIO_OUTPUT_OUTPUT].setVoltage(5.f * sine);
+
+		// Blink light at 1Hz
+		blinkPhase += args.sampleTime;
+		if (blinkPhase >= 1.f)
+			blinkPhase -= 1.f;
+		lights[END_LED_LIGHT].setBrightness(blinkPhase < 0.5f ? 1.f : 0.f);
 	}
 };
 
@@ -75,31 +103,31 @@ struct VareoWidget : ModuleWidget {
 		addChild(createWidget<ScrewSilver>(Vec(RACK_GRID_WIDTH, RACK_GRID_HEIGHT - RACK_GRID_WIDTH)));
 		addChild(createWidget<ScrewSilver>(Vec(box.size.x - 2 * RACK_GRID_WIDTH, RACK_GRID_HEIGHT - RACK_GRID_WIDTH)));
 
-		addParam(createParamCentered<RoundBlackKnob>(mm2px(Vec(5.0, 14.942)), module, Vareo::REC_BUTTON_PARAM));
-		addParam(createParamCentered<RoundBlackKnob>(mm2px(Vec(35.64, 14.942)), module, Vareo::JUMP_BUTTON_PARAM));
-		addParam(createParamCentered<RoundBlackKnob>(mm2px(Vec(20.32, 28.163)), module, Vareo::SPEED_KNOB_PARAM));
-		addParam(createParamCentered<RoundBlackKnob>(mm2px(Vec(9.765, 47.896)), module, Vareo::SPEED_ATTENUVERTER_PARAM));
-		addParam(createParamCentered<RoundBlackKnob>(mm2px(Vec(29.173, 51.837)), module, Vareo::DELAY_KNOB_PARAM));
-		addParam(createParamCentered<RoundBlackKnob>(mm2px(Vec(5.974, 62.966)), module, Vareo::JUMP_DIR_SWITCH_PARAM));
-		addParam(createParamCentered<RoundBlackKnob>(mm2px(Vec(34.895, 73.403)), module, Vareo::BLEND_MODE_SWITCH_PARAM));
-		addParam(createParamCentered<RoundBlackKnob>(mm2px(Vec(20.32, 74.117)), module, Vareo::BLEND_KNOB_PARAM));
-		addParam(createParamCentered<RoundBlackKnob>(mm2px(Vec(5.974, 82.955)), module, Vareo::JUMP_MODE_SWITCH_PARAM));
+		addParam(createParamCentered<VCVLatch>(mm2px(Vec(4.471, 16.529)), module, Vareo::REC_BUTTON_PARAM));
+		addParam(createParamCentered<VCVButton>(mm2px(Vec(36.169, 16.529)), module, Vareo::JUMP_BUTTON_PARAM));
+		addParam(createParamCentered<RoundHugeBlackKnob>(mm2px(Vec(20.32, 26.576)), module, Vareo::SPEED_KNOB_PARAM));
+		addParam(createParamCentered<Trimpot>(mm2px(Vec(9.765, 46.328)), module, Vareo::SPEED_ATTENUVERTER_PARAM));
+		addParam(createParamCentered<RoundBigBlackKnob>(mm2px(Vec(29.173, 50.779)), module, Vareo::DELAY_KNOB_PARAM));
+		addParam(createParamCentered<CKSS>(mm2px(Vec(6.503, 61.378)), module, Vareo::JUMP_DIR_SWITCH_PARAM));
+		addParam(createParamCentered<CKSS>(mm2px(Vec(34.646, 73.403)), module, Vareo::BLEND_MODE_SWITCH_PARAM));
+		addParam(createParamCentered<RoundBigBlackKnob>(mm2px(Vec(20.32, 74.117)), module, Vareo::BLEND_KNOB_PARAM));
+		addParam(createParamCentered<CKSS>(mm2px(Vec(6.503, 83.484)), module, Vareo::JUMP_MODE_SWITCH_PARAM));
 
-		addInput(createInputCentered<PJ301MPort>(mm2px(Vec(-24.82, -111.75)), module, Vareo::BLEND_INPUT_INPUT));
-		addInput(createInputCentered<PJ301MPort>(mm2px(Vec(-15.82, -111.75)), module, Vareo::DELAY_INPUT_INPUT));
-		addInput(createInputCentered<PJ301MPort>(mm2px(Vec(-6.82, -111.75)), module, Vareo::R_AUDIO_INPUT_INPUT));
-		addInput(createInputCentered<PJ301MPort>(mm2px(Vec(-24.82, -101.75)), module, Vareo::REC_INPUT_INPUT));
-		addInput(createInputCentered<PJ301MPort>(mm2px(Vec(-15.82, -101.75)), module, Vareo::SPEED_INPUT_INPUT));
-		addInput(createInputCentered<PJ301MPort>(mm2px(Vec(-6.82, -101.75)), module, Vareo::L_AUDIO_INPUT_INPUT));
-		addInput(createInputCentered<PJ301MPort>(mm2px(Vec(-24.82, -91.75)), module, Vareo::JUMP_INPUT_INPUT));
+		addInput(createInputCentered<PJ301MPort>(mm2px(Vec(25.349, 91.75)), module, Vareo::JUMP_INPUT_INPUT));
+		addInput(createInputCentered<PJ301MPort>(mm2px(Vec(5.762, 101.75)), module, Vareo::L_AUDIO_INPUT_INPUT));
+		addInput(createInputCentered<PJ301MPort>(mm2px(Vec(15.291, 101.75)), module, Vareo::SPEED_INPUT_INPUT));
+		addInput(createInputCentered<PJ301MPort>(mm2px(Vec(25.349, 101.75)), module, Vareo::REC_INPUT_INPUT));
+		addInput(createInputCentered<PJ301MPort>(mm2px(Vec(5.762, 111.75)), module, Vareo::R_AUDIO_INPUT_INPUT));
+		addInput(createInputCentered<PJ301MPort>(mm2px(Vec(15.291, 111.75)), module, Vareo::DELAY_INPUT_INPUT));
+		addInput(createInputCentered<PJ301MPort>(mm2px(Vec(25.349, 111.75)), module, Vareo::BLEND_INPUT_INPUT));
 
-		addOutput(createOutputCentered<PJ301MPort>(mm2px(Vec(-33.82, -111.75)), module, Vareo::R_AUDIO_OUTPUT_OUTPUT));
-		addOutput(createOutputCentered<PJ301MPort>(mm2px(Vec(-33.82, -101.75)), module, Vareo::L_AUDIO_OUTPUT_OUTPUT));
-		addOutput(createOutputCentered<PJ301MPort>(mm2px(Vec(-33.82, -91.75)), module, Vareo::END_OUTPUT_OUTPUT));
+		addOutput(createOutputCentered<PJ301MPort>(mm2px(Vec(34.878, 91.75)), module, Vareo::END_OUTPUT_OUTPUT));
+		addOutput(createOutputCentered<PJ301MPort>(mm2px(Vec(34.878, 101.75)), module, Vareo::L_AUDIO_OUTPUT_OUTPUT));
+		addOutput(createOutputCentered<PJ301MPort>(mm2px(Vec(34.878, 111.75)), module, Vareo::R_AUDIO_OUTPUT_OUTPUT));
 
-		addChild(createLightCentered<MediumLight<RedLight>>(mm2px(Vec(11.0, 14.942)), module, Vareo::REC_LED_LIGHT));
-		addChild(createLightCentered<MediumLight<RedLight>>(mm2px(Vec(29.64, 14.942)), module, Vareo::JUMP_TOLERANCE_LED_LIGHT));
-		addChild(createLightCentered<MediumLight<RedLight>>(mm2px(Vec(33.82, 84.332)), module, Vareo::END_LED_LIGHT));
+		addChild(createLightCentered<MediumLight<RedLight>>(mm2px(Vec(4.471, 22.507)), module, Vareo::REC_LED_LIGHT));
+		addChild(createLightCentered<MediumLight<GreenLight>>(mm2px(Vec(36.169, 22.507)), module, Vareo::JUMP_TOLERANCE_LED_LIGHT));
+		addChild(createLightCentered<MediumLight<BlueLight>>(mm2px(Vec(34.878, 84.332)), module, Vareo::END_LED_LIGHT));
 	}
 };
 
